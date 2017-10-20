4d2c6d4ad757a66b2134148ebb1bba84680e350d
Christian Zirpins <christian.zirpins@hs-karlsruhe.de>
2017-10-20 11:59:20 +0200

    clear lab2-4 for upcoming updates

--- lab3/genthrift.sh
+++
@@ -1,4 +0,0 @@
-rm -rf gen-javabean src/main/java/de/hska/iwi/bdelab/schema
-thrift -r --gen java:beans,hashcode,nocamel src/main/thrift/schema.thrift
-mv gen-javabean/de/hska/iwi/bdelab/schema src/main/java/de/hska/iwi/bdelab/
-rm -rf gen-javabean
