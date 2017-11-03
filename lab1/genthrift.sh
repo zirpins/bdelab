e5955624ce19a91e42c71070fd23dcaff6fef3c4
Christian Zirpins <christian.zirpins@hs-karlsruhe.de>
2017-11-03 09:23:58 +0100

    prevent race condition

--- lab1/genthrift.sh
+++ lab1/genthrift.sh
@@ -1,4 +1,5 @@
 rm -rf gen-javabean src/main/java/de/hska/iwi/bdelab/schema
 thrift -r --gen java:beans,hashcode,nocamel src/main/thrift/schema.thrift
+sleep 3
 mv gen-javabean/de/hska/iwi/bdelab/schema src/main/java/de/hska/iwi/bdelab/
 rm -rf gen-javabean
